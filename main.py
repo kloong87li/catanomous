dc6dfa17375c952ea29bac5e468e11ec1a7be017
Kenneth Li <kloong87i@gmail.com>
2016-03-25 14:36:35 -0400

    working android tests

--- main.py
+++ main.py
@@ -46,21 +46,50 @@ def detect_image(img):
 
 
 def main():
-  imgs = ["1536x2048.jpg","2992x4000_1.jpg",
+  android_dir = "images/android/"
+  android_imgs = ["1536x2048.jpg","2992x4000_1.jpg",
           "2992x4000_2.jpg", "2992x4000_3.jpg",
           "2992x4000_4.jpg", "3232x2416_1.jpg",
           "3232x2416_2.jpg", "3232x2416_3.jpg",
           "3232x2416_4.jpg"]
-
-  test_imgs = [
-    "1536x2048.jpg", "2992x4000_1.jpg","2992x4000_2.jpg", "2992x4000_3.jpg",
-    "3232x2416_4.jpg"
+  android_test = [
+          "1536x2048.jpg", "2992x4000_1.jpg",
+          "2992x4000_2.jpg", "2992x4000_3.jpg",
+          "3232x2416_4.jpg"
   ]
-  
-  for img in test_imgs:
-    print img
-    detect_image("images/" + img)
 
+  
+  
+  test_dir = "images/"
+  test1_imgs = [
+    "test1_1.jpg",
+    "test1_2.jpg",
+    "test1_3.jpg",
+    "test1_4.jpg",
+    "test1_5.jpg",
+    "test1_6.jpg",
+  ]
+  test2_imgs = [
+    "test2_1.jpg",
+    "test2_2.jpg",
+  ]
+  test3_imgs = [
+    "test3_1.jpg",
+    "test3_2.jpg",
+  ]
+  test4_imgs = [
+    "test4_1.jpg",
+    "test4_2.jpg",
+    "test4_3.jpg",
+  ]
+
+  # for img in test4_imgs:
+  #   print img
+  #   detect_image(test_dir + img)
+
+  for img in android_test:
+    print img
+    detect_image(android_dir + img)
   
 
   
