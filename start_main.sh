584078d5cdbb4b143bbff5cc4886a8de8f933029
Ken <kloong87li@gmail.com>
2016-04-28 03:42:10 -0500

    start script changes

--- start_main.sh
+++ start_main.sh
@@ -2,5 +2,5 @@
 # start_main.sh
 # enables cv environment and launches main controller for catanomous
 
-~/.virtualenvs/cv/bin/activate
-sudo ~/.virtualenvs/cv/bin/python ~/catanomous/main.py -a
+. /home/pi/.virtualenvs/cv/bin/activate
+sudo /home/pi/.virtualenvs/cv/bin/python /home/pi/catanomous/main.py -a
