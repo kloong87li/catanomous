c412fec9c1187007beaeb3347950c865d7b6c1ff
Kenneth Li <kloong87i@gmail.com>
2016-04-28 22:10:56 -0400

    gpio stuff

---
+++ catan/detection/gpio_controller.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+class GPIOController(object):
+
+  def __init__(self):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.OUT)
+    return
+
+
+  def init_button(self, pin_num):
+    GPIO.setup(pin_num, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    return
+
+
+  def wait_for_press(self, pin_num):
+    while GPIO.input(pin_num):
+      time.sleep(.2)
+    
+    return
+
+
+  def wait_for_presses(self, pin_num, num_presses):
+    for i in xrange(len(num_presses)):
+      self.wait_for_press(pin_num)
+
+    return
+
+
+  def led_on(self):
+    GPIO.output(16, GPIO.LOW)
+
+  def led_off(self):
+    GPIO.output(16, GPIO.HIGH)
+
+  def led_blink(self, num=1):
+    self.led_off()
+    time.sleep(.3)
+    self.led_on()
+    time.sleep(.3)
+    self.led_off()
+
