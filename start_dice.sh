6c3f5ba8491dcec50312cc18b95d725bd0daf5f5
Kenneth Li <kloong87i@gmail.com>
2016-04-29 18:05:20 -0400

    dice controller updates

--- start_dice.sh
+++ start_dice.sh
@@ -3,6 +3,6 @@
 # enables cv environment and launches dice controller for catanomous
 
 export PYTHONPATH=${PYTHONPATH}:/usr/local/lib/python2.7/site-packages
-. /home/.virtualenvs/cv/bin/activate
+. /home/pi/.virtualenvs/cv/bin/activate
 cd /home/pi/catanomous
-sudo /home/.virtualenvs/cv/bin/python dice_main.py -a
+sudo /home/pi/.virtualenvs/cv/bin/python dice_main.py -a
